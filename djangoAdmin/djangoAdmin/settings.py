67afb74c5907422ec7327eca5e93e8e062d2b011
rlpdcj <rlpdcj@gmail.com>
2017-11-07 20:57:48 -0200

    senha mysql

--- djangoAdmin/djangoAdmin/settings.py
+++ djangoAdmin/djangoAdmin/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangoAdmin',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '@Rj29051971*',
         'HOST': 'localhost',
         'PORT': '3306',
        }
